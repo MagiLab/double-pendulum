6f4cb740610c4313a95f3e6089cc929a08ca5fa6
MagiLab <80799290+MagiLab@users.noreply.github.com>
2026-05-28 16:38:27 +1200

    fix: add error checking for shader compiler and program link

--- script.js
+++ script.js
@@ -390,6 +390,9 @@ function createShader(type, source) {
   const shader = gl.createShader(type);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error("Shader compile error: ", gl.getShaderInfoLog(shader));
+  }
   return shader;
 }
 
@@ -398,6 +401,9 @@ function createProgram(vs, fs) {
   gl.attachShader(program, vs);
   gl.attachShader(program, fs);
   gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error("Program link error: ", gl.getProgramInfoLog(program));
+  }
   return program;
 }
 
